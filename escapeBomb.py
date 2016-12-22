dc7a7865f7e6554f5fe1f75fa61514de9ede2ab4
RocKIn0X <inman27@hotmail.com>
2016-12-22 17:39:09 +0700

    add Grid & delete ModelSprite

--- escapeBomb.py
+++ escapeBomb.py
@@ -1,25 +1,11 @@
 import arcade
 
-from models import World, Player
+from models import World, Player, Grid
 
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 
 SPRITE_SCALING = 0.25
-
-class ModelSprite(arcade.Sprite):
-    def __init__(self, *args, **kwargs):
-        self.model = kwargs.pop('model', None)
-
-        super().__init__(*args, SPRITE_SCALING, **kwargs)
-
-    def sync_with_model(self):
-        if self.model:
-            self.set_position(self.model.x, self.model.y)
-
-    def draw(self):
-        self.sync_with_model()
-        super().draw()
 
 class EscapeBombWindow(arcade.Window):
     def __init__(self, width, height):
@@ -28,15 +14,15 @@ class EscapeBombWindow(arcade.Window):
         arcade.set_background_color(arcade.color.GRAY)
 
         self.world = World(width, height)
-        self.player_sprite = ModelSprite('images/boy.png', model=self.world.player)
-
-    def animate(self, delta):
-        self.world.animate(delta)
+        self.grid = Grid(self.world)
 
     def on_draw(self):
         arcade.start_render()
 
-        self.player_sprite.draw()
+        self.world.draw()
+
+    def on_key_press(self, key, key_modifiers):
+        self.world.on_key_press(key, key_modifiers)
 
 if __name__ == '__main__':
     window = EscapeBombWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
--- models.py
+++ models.py
@@ -1,23 +1,68 @@
 import arcade.key
 from random import randint, random
 
+WIDTH = 40
+HEIGHT = 40
+
+MARGIN = 10
+
 class Player:
-    def __init__(self, world, x, y):
+    def __init__(self, world, grid):
         self.world = world
-        self.x = x
-        self.y = y
+        self.grid = grid
 
     def animate(self, delta):
         if self.y > self.world.height:
             self.y = 0
         self.y += 5
 
+class Grid():
+    def __init__(self, world):
+        self.world = world
+        self.grid = []
+
+        for row in range(10):
+            # Add an empty array that will hold each cell
+            # in this row
+            self.grid.append([])
+            for column in range(10):
+                self.grid[row].append(0)  # Append a cell
+
+    def on_draw(self):
+        # Draw the grid
+        for row in range(10):
+            for column in range(10):
+                # Figure out what color to draw the box
+                if self.grid[row][column] == 1:
+                    color = arcade.color.GREEN
+                else:
+                    color = arcade.color.WHITE
+
+                # Do the math to figure out where the box is
+                x = (MARGIN + WIDTH) * column + MARGIN + WIDTH // 2 + 385
+                y = (MARGIN + HEIGHT) * row + MARGIN + HEIGHT // 2 + 105
+
+                # Draw the box
+                arcade.draw_rectangle_filled(x, y, WIDTH, HEIGHT, color)
+
+
 class World:
     def __init__(self, width, height):
         self.width = width
         self.height = height
 
-        self.player = Player(self, 100, 100)
+        self.grid = Grid(self)
+        self.player = Player(self, self.grid)
 
     def animate(self, delta):
         self.player.animate(delta)
+
+    def draw(self):
+        self.grid.on_draw()
+
+    def on_key_press(self, key, key_modifiers):
+        if key == arcade.key.W:
+            print("hello")
+
+        if key == arcade.key.D:
+            print("world")
