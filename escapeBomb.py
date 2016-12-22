7f5ded3bb1c027e2373733c34a759d00d4e4ae92
RocKIn0X <inman27@hotmail.com>
2016-12-22 13:57:17 +0700

    add ModelSprite

--- escapeBomb.py
+++ escapeBomb.py
@@ -7,18 +7,31 @@ SCREEN_HEIGHT = 720
 
 SPRITE_SCALING = 0.25
 
+class ModelSprite(arcade.Sprite):
+    def __init__(self, *args, **kwargs):
+        self.model = kwargs.pop('model', None)
+
+        super().__init__(*args, SPRITE_SCALING, **kwargs)
+
+    def sync_with_model(self):
+        if self.model:
+            self.set_position(self.model.x, self.model.y)
+
+    def draw(self):
+        self.sync_with_model()
+        super().draw()
+
 class EscapeBombWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
 
         arcade.set_background_color(arcade.color.GRAY)
 
-        self.player_sprite = arcade.Sprite('images/boy.png', SPRITE_SCALING)
         self.world = World(width, height)
+        self.player_sprite = ModelSprite('images/boy.png', model=self.world.player)
 
     def animate(self, delta):
         self.world.animate(delta)
-        self.player_sprite.set_position(self.world.player.x, self.world.player.y)
 
     def on_draw(self):
         arcade.start_render()
