f677f6419cd61db946f48def23c70cd9ea71586e
RocKIn0X <inman27@hotmail.com>
2016-12-23 13:28:20 +0700

    add Win screen

--- escapeBomb.py
+++ escapeBomb.py
@@ -9,7 +9,7 @@ SPRITE_SCALING = 0.25
 
 STATE_GAME = "ON GAME"
 STATE_GAMEOVER = "GAME OVER"
-#STATE_WINNING = "WIN"
+STATE_WINNING = "WIN"
 
 class EscapeBombWindow(arcade.Window):
     def __init__(self, width, height):
@@ -25,6 +25,7 @@ class EscapeBombWindow(arcade.Window):
         self.grid = Grid(self.world)
         self.total_time = 1
         self.score = 0
+        self.checkEnd = 0
         self.bg_sound = arcade.load_sound("sounds/bg.ogg")
         #self.final_sound = arcade.load_sound("sounds/FLTFINAL.ogg")
 
@@ -36,15 +37,23 @@ class EscapeBombWindow(arcade.Window):
 
     def on_draw(self):
         arcade.start_render()
+        self.score = self.world.getScore()
 
         if self.state == STATE_GAME:
             self.world.draw()
             if self.total_time <= 0:
                 self.state = STATE_GAMEOVER
+            if self.score >= 10:
+                self.total_time = 0
+                self.state = STATE_WINNING
+        elif self.state == STATE_WINNING:
+            arcade.set_background_color(arcade.color.YELLOW)
+            arcade.draw_text("You can escape the bomb \^o^/" , 300, 400, arcade.color.BLUE, 40)
+            arcade.draw_text("Your score: " + str(self.score), 500, 300, arcade.color.BLACK, 40)
+            arcade.draw_text("Click for restart game", 560, 240, arcade.color.BLACK, 20)
 
         else:
             arcade.set_background_color(arcade.color.YELLOW)
-            self.score = self.world.getScore()
             arcade.draw_text("Your score: " + str(self.score), 500, 360, arcade.color.BLACK, 40)
             arcade.draw_text("Click for restart game", 560, 300, arcade.color.BLACK, 20)
 
@@ -52,7 +61,9 @@ class EscapeBombWindow(arcade.Window):
         if self.state == STATE_GAME:
             self.world.on_mouse_press(x, y, button, modifiers)
         else:
-            self.setup(SCREEN_WIDTH, SCREEN_HEIGHT)
+            self.checkEnd += 1
+            if self.checkEnd == 2:
+                self.setup(SCREEN_WIDTH, SCREEN_HEIGHT)
 
 
 if __name__ == '__main__':
